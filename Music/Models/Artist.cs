28b30b715cba4d51e184ce285d2ea4ab306acd8a
hoanem <hoanem@gmail.com>
2016-04-04 17:39:25 -0400

    Initial Commit

--- Music/Models/Artist.cs
+++ Music/Models/Artist.cs
@@ -1,8 +1,11 @@
-﻿namespace Music.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Music.Models
 {
     public class Artist
     {
         public int ArtistID { get; set; }
+        [Required(ErrorMessage = "Artist name is required")]
         public string Name { get; set; }
     }
 }
